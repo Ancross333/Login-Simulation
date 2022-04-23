a215b2ddb0120039315195adddb4ae67149ccee6
Ancross333 <101778926+Ancross333@users.noreply.github.com>
2022-04-23 13:47:54 -0700

    Delete LoginSimulation directory

--- LoginSimulation/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.loginsimulation {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.loginsimulation to javafx.fxml;
-    exports com.example.loginsimulation;
-}
